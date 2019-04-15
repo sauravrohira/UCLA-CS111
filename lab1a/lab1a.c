f79057d5ce22ade8d0b5c5ecb9b570ec7cdea66e
Saurav Rohira <saurav.rohira@engineering.ucla.edu>
2019-04-14 21:10:35 -0700

    final possibly

--- lab1a/lab1a.c
+++ lab1a/lab1a.c
@@ -12,36 +12,54 @@
 #include <termios.h>
 #include <poll.h>
 
+//Global Constants:
 #define BUF_SIZE 256
 #define KEYBOARD 0
 #define SHELL 1
 
+//Global Variables:
 struct termios default_mode; //stores the current terminal mode
 struct termios project_mode; //stores modified mode for project
 int shell_flag = 0; //flag used to identify if shell option was provided
-int shell_in[2];
-int shell_out[2];
-char* buf;
-int buf_len = 0;
-struct pollfd readPoll[2];
-pid_t child_pid = 0;
-short poll_events = (POLL_IN | POLL_ERR | POLL_HUP);
+int shell_in[2]; //stores file descriptors for pipe to shell
+int shell_out[2]; //stores file descriptors for pipe from shell
+char* buf; //buffer used for reading from shell and keyboard and writing to shell and screen
+int buf_len = 0; //stores number of bytes read by most recent read call
+struct pollfd readPoll[2]; //array of pollfd structs used to set up polling
+pid_t shell_pid = 0; //stores pid of child process that runs the shell
+short poll_events = (POLL_IN | POLL_ERR | POLL_HUP); //events setting used by poll structures
 
+//Function called when the shell creates a SIGPIPE:
 void handleSignal(int signal_val)
 {
     if(signal_val == SIGPIPE)
         exit(0);
 }
 
-void redirectFD(int old_fd, int new_fd)
+//Calls close and prints error message on failure:
+void callClose(int fd)
 {
-    close(old_fd);
-    dup2(new_fd, old_fd);
+    if (close(fd) < 0)
+    {
+        fprintf(stderr, "Error: %s", strerror(errno));
+        exit(1);
+    }
 }
 
+//Used to redirect input and output:
+void redirectFD(int old_fd, int new_fd)
+{
+    callClose(old_fd);
+    if(dup2(new_fd, old_fd) < 0)
+    {
+        fprintf(stderr, "Error: %s", strerror(errno));
+        exit(1);
+    }
+}
+
+//Sets up the shell and pipes for communication with the shell:
 void setupShell()
 {
-
     if(signal(SIGPIPE, handleSignal) == SIG_ERR)
     {
         fprintf(stderr, "Error: %s", strerror(errno));
@@ -50,19 +68,19 @@ void setupShell()
     
     pipe(shell_in);
     pipe(shell_out);
-    child_pid = fork();
+    shell_pid = fork();
 
-    if (child_pid == -1)
+    if (shell_pid == -1)
     {
         fprintf(stderr, "Error: %s", strerror(errno));
         exit(1);
     }
 
-    if(child_pid == 0)
+    if(shell_pid == 0)
     {
 
-        close(shell_in[1]);
-        close(shell_out[0]);
+        callClose(shell_in[1]);
+        callClose(shell_out[0]);
         redirectFD(STDIN_FILENO, shell_in[0]);
         redirectFD(STDOUT_FILENO, shell_out[1]);
         redirectFD(STDERR_FILENO, shell_out[1]);
@@ -77,39 +95,45 @@ void setupShell()
         }
     }
 
-    if(child_pid > 0)
+    if(shell_pid > 0)
     {
-        close(shell_in[0]);
-        close(shell_out[1]);
+        callClose(shell_in[0]);
+        callClose(shell_out[1]);
     }
 }
 
+//Set terminal to character-at-a-time, no-echo mode:
 void setupTerminal()
 {
-    tcgetattr(0, &default_mode);
-    project_mode = default_mode;
+    tcgetattr(0, &default_mode); //Regular mode stored in default_mode.
+    project_mode = default_mode; 
     project_mode.c_cflag = ISTRIP;
     project_mode.c_oflag = 0;
     project_mode.c_lflag = 0;
-    tcsetattr(0, TCSANOW, &project_mode);
+    tcsetattr(0, TCSANOW, &project_mode); //New modified mode is made active.
 }
 
+//Function called upon exit, sets terminal back to it's default
 void restoreTerminal()
 {
     free(buf);
     tcsetattr(0, TCSANOW, &default_mode);
+
+    //prints shell exit status:
     if(shell_flag == 1)
     {
         int exit_status;
-        if(waitpid(child_pid, &exit_status, 0) < 0)
+        if(waitpid(shell_pid, &exit_status, 0) < 0)
         {
             fprintf(stderr, "Error: %s", strerror(errno));
+            exit(1);
         }
         if(WIFEXITED(exit_status))
             fprintf(stderr, "SHELL EXIT SIGNAL=%d STATUS=%d\n", WTERMSIG(exit_status), WEXITSTATUS(exit_status));
     }
 }
 
+//Calls read and prints error message on failure:
 void callRead(int fd, char* buf, size_t num_bytes)
 {
     buf_len = read(fd, buf, num_bytes);
@@ -120,6 +144,7 @@ void callRead(int fd, char* buf, size_t num_bytes)
     }
 }
 
+//Calls write and prints error message on failure:
 void callWrite(int fd, char* buf, size_t num_bytes)
 {
     if (write(fd, buf, num_bytes) == -1)
@@ -129,6 +154,7 @@ void callWrite(int fd, char* buf, size_t num_bytes)
     }
 }
 
+//Processes characters read from keyboard and shell and prints them:
 void processedWrite(int source) 
 {
     for(int i = 0; i < buf_len; i++)
@@ -138,7 +164,7 @@ void processedWrite(int source)
             case 3:
                 if(shell_flag)
                 {
-                    if(kill(child_pid, SIGINT) < 0)
+                    if(kill(shell_pid, SIGINT) < 0)
                     {
                         fprintf(stderr, "Error: %s", strerror(errno));
                         exit(1);
@@ -148,7 +174,7 @@ void processedWrite(int source)
             case 4:
             {
                 if(shell_flag == 1)
-                    close(shell_in[1]);
+                    callClose(shell_in[1]);
                 exit(0);
             }
                 break;
@@ -167,6 +193,7 @@ void processedWrite(int source)
     }
 }
 
+//Infinite loop for echo-only mode:
 void readMode()
 {
     while(1)
@@ -176,6 +203,7 @@ void readMode()
     }
 }
 
+//Infinite loop for echo and shell mode:
 void pollMode()
 {
     readPoll[0].fd = STDIN_FILENO;
@@ -205,13 +233,17 @@ void pollMode()
             callRead(shell_out[0], buf, BUF_SIZE);
             processedWrite(SHELL);
         }
+
+        if(readPoll[1].revents & (POLL_ERR | POLL_HUP))
+            exit(0);
+        
     }
 }
 
 int main(int argc, char ** argv)
 {
-    setupTerminal();
-    atexit(restoreTerminal);
+    setupTerminal(); //sets up new terminal mode.
+    atexit(restoreTerminal); //sets function to be called on exit.
 
     //variable used to store getopt_long return values
     int c;
@@ -226,14 +258,16 @@ int main(int argc, char ** argv)
     while (1)
     {
         c = getopt_long(argc, argv, "", longopts, &option_index);
-        if(c == -1)
+        if(c == -1) //breaks out of loop once all options have been parsed.
             break;
-        if(c != 0)
+        if(c != 0) //exits with code 1, when a incorrect option is provided.
             exit(1);
     }
 
+    //initialize buffer for reads and writes:
     buf = (char*) malloc(BUF_SIZE);
 
+    //chooses mode to run in:
     if(shell_flag == 1)
     {
         setupShell();
@@ -241,5 +275,7 @@ int main(int argc, char ** argv)
     }
     else
         readMode();
+
+    exit(0);
 }
 
